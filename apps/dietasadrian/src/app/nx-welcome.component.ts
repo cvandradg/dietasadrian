ce97ddea31a54b1a8562a277f50925f4681ceb4a
claudio <cvan95@outlook.com>
2022-09-22 00:10:32 -0600

    fix: check deploy on merge

--- apps/dietasadrian/src/app/nx-welcome.component.ts
+++ apps/dietasadrian/src/app/nx-welcome.component.ts
@@ -433,7 +433,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
         <div id="welcome">
           <h1>
             <span> Hello there, </span>
-            Welcome dietasadrian test after deploy 44444👋
+            Welcome dietasadrian test after deploy 5555👋
           </h1>
         </div>
 
