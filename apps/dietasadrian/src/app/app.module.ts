e9bbc588c069e36c604b64b5ad9e298f601c7e20
claudio <cvan95@outlook.com>
2022-09-24 01:32:49 -0600

    feat: material design working incorrectly

--- apps/dietasadrian/src/app/app.module.ts
+++ apps/dietasadrian/src/app/app.module.ts
@@ -6,11 +6,21 @@ import { AppComponent } from './app.component';
 
 import ROUTES from "./routes";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';0
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, RouterModule.forRoot(ROUTES), BrowserAnimationsModule],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES), BrowserAnimationsModule, MatToolbarModule, MatIconModule, MatProgressSpinnerModule, MatExpansionModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
