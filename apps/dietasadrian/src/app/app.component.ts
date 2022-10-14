a7eed45a47151d908f4958ccc168255377dccc6c
claudio <cvan95@outlook.com>
2022-10-13 18:50:53 -0600

    fix: unnecessary interface

--- apps/dietasadrian/src/app/app.component.ts
+++ apps/dietasadrian/src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 
-interface Food {
-  value: string;
-  viewValue: string;
-}
-
 @Component({
   selector: 'dietas-adrian-nx-workspace-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-  // test
   panelOpenState = false;
 
   title = 'dietasadrian';
