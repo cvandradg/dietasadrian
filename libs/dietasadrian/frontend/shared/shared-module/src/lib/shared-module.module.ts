31de4abb9ab1e22a0f843acf138a85464691c9ed
claudio <cvan95@outlook.com>
2022-10-16 16:02:05 -0600

    fix: meals list layout

--- libs/dietasadrian/frontend/shared/shared-module/src/lib/shared-module.module.ts
+++ libs/dietasadrian/frontend/shared/shared-module/src/lib/shared-module.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
 
 const MODULES = [
   MatToolbarModule,
   MatIconModule,
   MatButtonModule,
+  DragDropModule
 ]
 
 @NgModule({
--- libs/dietasadrian/frontend/ui/dietas/src/lib/components/create-diets/create-diets.component.ts
+++ libs/dietasadrian/frontend/ui/dietas/src/lib/components/create-diets/create-diets.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit, NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CdkDragDrop, moveItemInArray, } from '@angular/cdk/drag-drop';
+import { SharedModuleModule } from '@shared-modules';
 
 @Component({
   selector: 'dietas-adrian-nx-workspace-create-diets',
   templateUrl: './create-diets.component.html',
   styleUrls: ['./create-diets.component.scss'],
 })
-export class CreateDietsComponent implements OnInit {
+export class CreateDietsComponent {
 
-  ngOnInit(): void {
-    console.log();
-    
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode I - The Phantom Menace',
+
+
+  ];
+
+  //Desied type CdkDragDrop<string[]>
+  drop(event: any) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
+
 }
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModuleModule],
   declarations: [CreateDietsComponent],
   exports: [CreateDietsComponent],
 })
