8cf9cf19495012a677cd73d0f85d01f287878fc1
claudio <cvan95@outlook.com>
2022-10-18 00:26:14 -0600

    feat: shadow fox and chips

--- libs/dietasadrian/frontend/ui/dietas/src/lib/components/create-diets/create-diets.component.ts
+++ libs/dietasadrian/frontend/ui/dietas/src/lib/components/create-diets/create-diets.component.ts
@@ -1,7 +1,14 @@
 import { Component, NgModule } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { MatChipInputEvent } from '@angular/material/chips';
+
 import { CommonModule } from '@angular/common';
-import { CdkDragDrop, moveItemInArray, } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { SharedModuleModule } from '@shared-modules';
+
+export interface Fruit {
+  name: string;
+}
 
 @Component({
   selector: 'dietas-adrian-nx-workspace-create-diets',
@@ -9,21 +16,44 @@ import { SharedModuleModule } from '@shared-modules';
   styleUrls: ['./create-diets.component.scss'],
 })
 export class CreateDietsComponent {
+  movies = ['Episode I - The Phantom Menace'];
 
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode I - The Phantom Menace',
-
-
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  fruits: Fruit[] = [
+    { name: 'pollo' },
+    { name: 'arroz' },
+    { name: 'frijoles' },
   ];
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.fruits.push({ name: value });
+    }
+
+    // Clear the input value
+    event.chipInput?.clear();
+  }
+
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
+  }
+
+  addMeal() {
+    this.movies.push('random movie');
+  }
 
   //Desied type CdkDragDrop<string[]>
   drop(event: any) {
     moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
   }
-
 }
 
 @NgModule({
