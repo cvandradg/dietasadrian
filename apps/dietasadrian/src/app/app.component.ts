7e2cf0c8c71600b189d109983b54c6e423ddef0d
claudio <cvan95@outlook.com>
2022-10-24 18:57:18 -0600

    feat: title logo and other simplifications

--- apps/dietasadrian/src/app/app.component.ts
+++ apps/dietasadrian/src/app/app.component.ts
@@ -5,8 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  panelOpenState = false;
-
-  title = 'dietasadrian';
-}
+export class AppComponent {}
