0d4c75d1fd0dab6e4bb54bd21d0518271e767f56
claudio <cvan95@outlook.com>
2022-11-14 17:57:16 -0600

    fix: icons of add ingredient makes more sense css wise

---
+++ libs/dietasadrian/frontend/shared/shared-module/src/lib/export-components.ts
@@ -0,0 +1,7 @@
+import { AddIngredientDialogComponentModule } from '@dialogs/add-ingredient-dialog/add-ingredient-dialog.component';
+import { ConfirmationDialogComponentModule } from '@dialogs/confirmation-dialog/confirmation-dialog.component';
+
+export const COMPONENTS = [
+  ConfirmationDialogComponentModule,
+  AddIngredientDialogComponentModule,
+];
