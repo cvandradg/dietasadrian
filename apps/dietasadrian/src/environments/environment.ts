d0212c391c2f5893274c82a1cd4305625e298086
claudio <54946311+cvandradg@users.noreply.github.com>
2023-05-13 21:16:33 -0600

    feat: gradient effect on validators

--- apps/dietasadrian/src/environments/environment.ts
+++ apps/dietasadrian/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyAnZfF6TYw1ubCSkV8RhClrm8RjVLqqGlE',
+    authDomain: 'dietasadrianbadillafirebase.firebaseapp.com',
+    databaseURL:
+      'https://dietasadrianbadillafirebase-default-rtdb.firebaseio.com',
+    projectId: 'dietasadrianbadillafirebase',
+    storageBucket: 'dietasadrianbadillafirebase.appspot.com',
+    messagingSenderId: '706318825388',
+    appId: '1:706318825388:web:9fe85e9af68b552359ac09',
+  },
   production: false,
 };
 
--- libs/dietasadrian/frontend/ui/app/src/lib/components/login/login.component.ts
+++ libs/dietasadrian/frontend/ui/app/src/lib/components/login/login.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SharedModuleModule } from '@shared-modules';
+import { FormBuilder, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'dietas-adrian-nx-workspace-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [HeaderComponent, SharedModuleModule],
+  imports: [CommonModule,HeaderComponent, SharedModuleModule],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
+  loginInputForm = this.formBuilder.group({
+    user: [
+      '',
+      [
+        Validators.required, // Validators
+        Validators.min(5),
+        Validators.max(30),
+      ],
+    ],
+    pass: [
+      '',
+      [
+        Validators.required, // Validators
+        Validators.min(5),
+        Validators.max(30),
+      ],
+    ],
+  });
+
+  onSubmit() {
+    // some operations here
+  }
+}
