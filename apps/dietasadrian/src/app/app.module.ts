afa1dae1991bb31b89743164fd0795aaab66b170
claudio <54946311+cvandradg@users.noreply.github.com>
2023-06-01 15:37:01 -0600

    feat: root store to access the feature store

--- apps/dietasadrian/src/app/app.module.ts
+++ apps/dietasadrian/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideRouter, RouterModule, withRouterConfig } from '@angular/router';
 
@@ -6,10 +6,33 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from '../routes';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import * as fromRootStore from './+state/root-store.reducer';
+import { RootStoreEffects } from './+state/root-store.effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, RouterModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    StoreModule.forRoot(
+      {},
+      {
+        metaReducers: [],
+        runtimeChecks: {
+          strictActionImmutability: true,
+          strictStateImmutability: true,
+        },
+      }
+    ),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({ logOnly: !isDevMode() }),
+  ],
   providers: [
     provideRouter(
       routes,
