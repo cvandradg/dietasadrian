f49cc4359edf44854a0edf58e3ad3f349d5747f6
claudio <54946311+cvandradg@users.noreply.github.com>
2023-06-05 04:26:17 -0600

    fix: attempt to fix

--- apps/dietasadrian/src/main.ts
+++ apps/dietasadrian/src/main.ts
@@ -11,3 +11,11 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
+
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { appConfig } from './app/app.config';
+// import { AppComponent } from './app/app.component';
+
+// bootstrapApplication(AppComponent, appConfig).catch((err) =>
+//   console.error(err)
+// );
