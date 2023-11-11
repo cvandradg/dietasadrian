fcc11bfd8b09b433834df1bc5192bce9f96af487
claudio <54946311+cvandradg@users.noreply.github.com>
2023-11-11 01:01:28 -0600

    feat: client lib creation

---
+++ libs/dietasadrian/frontend/ui/client/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { ClientComponent } from './components/client/client.component';
+
+export const clientRoutes: Route[] = [{ path: '', component: ClientComponent }];
