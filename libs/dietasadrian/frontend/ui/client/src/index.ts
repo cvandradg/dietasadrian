8ab223911a14f1068768e72a365dd4efe61ea844
claudio <54946311+cvandradg@users.noreply.github.com>
2023-11-15 21:05:57 -0600

    feat: router link and simplification on client options

--- libs/dietasadrian/frontend/ui/client/src/index.ts
+++ libs/dietasadrian/frontend/ui/client/src/index.ts
@@ -7,3 +7,5 @@ export * from './lib/components/profile/profile.component';
 export * from './lib/components/utilities/utilities.component';
 
 export * from './lib/components/diets/diets.component';
+
+export * from './lib/components/utilities/utilities.component';
--- libs/dietasadrian/frontend/ui/client/src/lib/components/utilities/utilities.component.ts
+++ libs/dietasadrian/frontend/ui/client/src/lib/components/utilities/utilities.component.ts
@@ -2,11 +2,11 @@ import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
+  selector: 'lib-utilities',
   standalone: true,
   imports: [CommonModule],
-  selector: 'lib-utilities',
   templateUrl: './utilities.component.html',
   styleUrls: ['./utilities.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UtilitiesComponent {}
+export class UtilitiesComponent { }
