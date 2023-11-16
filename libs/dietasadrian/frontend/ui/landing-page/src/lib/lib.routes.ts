00f2c52a166438941cf47c51b6d73af731c3045c
claudio <54946311+cvandradg@users.noreply.github.com>
2023-11-16 14:33:13 -0600

    feat: newer version of landing-page lib

---
+++ libs/dietasadrian/frontend/ui/landing-page/src/lib/lib.routes.ts
@@ -0,0 +1,6 @@
+import { Route } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+
+export const landingPageRoutes: Route[] = [
+  { path: '', component: LandingPageComponent },
+];
